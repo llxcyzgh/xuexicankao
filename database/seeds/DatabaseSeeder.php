7f2b86df91e7dd78bdc76ef6eaf97c065a19066b
zhujianghan <zhujianghan@gmail.com>
2019-05-18 18:27:13 +0800

    create questions table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(\Encore\Admin\Auth\Database\AdminTablesSeeder::class);
+
+        $this->call(QuestionsTableSeeder::class);
+
     }
 }
